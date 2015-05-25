47c847239df0d21ad2fe51ce3505c71717a82baa
Richer Balázs <richer.balazs@gmail.com>
2015-05-25 12:24:50 +0200

    Initial commit + koncokout wiredep + playground basics

---
+++ app/scripts/main.js
@@ -0,0 +1,55 @@
+var ViewDatas = function() {
+    var _this = this;
+
+    this.players = ko.observableArray([]);
+
+    this.refreshPlayer = function(playerScore) {
+        var match = ko.utils.arrayFirst(this.players(), function(item) {
+            return playerScore.name === item.name;
+        });
+
+        if (match) {
+            match.score(match.score() - playerScore.score)
+        }
+    }
+};
+
+
+var playerModel = function(name, status, score) {
+    this.name = name;
+    this.status = status;
+    this.score = ko.observable(score)
+}
+
+
+var viewDatas = new ViewDatas();
+
+$(function() {
+    ko.applyBindings({
+        players: viewDatas.players
+    });
+
+    viewDatas.players.push(new playerModel('Eszti', 1, 301));
+    viewDatas.players.push(new playerModel('Balázs', 0, 301));
+
+    $("#dartboard #areas g").children().click(function(){
+        var id;
+        var score;
+
+        id = $(this).attr('id');
+        
+        score = id.substring(1);
+
+        
+        if (id[0] == 'd') {
+            score = score * 2
+        }
+
+        if (id[0] == 't') {
+            score = score * 3
+        }
+
+        viewDatas.refreshPlayer({name: "Eszti", score:score});
+
+    });
+});
