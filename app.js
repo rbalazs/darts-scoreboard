0df96128661259198adc1c1c3b5e5df0a059c99e
Richer Balázs <richer.balazs@gmail.com>
2016-09-30 19:04:03 +0200

    Heroku deploy vol.2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/app'));
 app.listen(port);
