8719545b3a55ec6f72cf9706df1e28eab1997e81
Richer Balázs <richer.balazs@gmail.com>
2016-10-01 09:55:57 +0200

    Startup script with menu.

--- start.sh
+++ start.sh
@@ -1,4 +1,45 @@
 #!/bin/bash
+ 
+# clear the screen
+tput clear
+ 
+# Move cursor to screen location X,Y (top left is 0,0)
+tput cup 3 15
+ 
+# Set a foreground colour using ANSI escape
+tput setaf 3
+echo "##darts-scoreboard##"
+tput sgr0
+ 
+tput cup 5 17
+# Set reverse video mode
+tput rev
+echo "M A I N - M E N U"
+tput sgr0
+ 
+tput cup 7 15
+echo "1. Start"
+ 
+tput cup 8 15
+echo "2. Reset database"
+ 
+tput cup 9 15
+echo "3. Run tests"
+ 
+tput cup 10 15
+echo "4. Remove npm and node modules"
+ 
+# Set bold mode 
+tput bold
+tput cup 12 15
+read -p "Enter your choice [1-4] " choice
+ 
+tput clear
+tput sgr0
+tput rc
 
-npm install &&
-grunt serve
+if [ $choice = 1 ]
+   then
+        echo "Starting up application.."
+        npm install && grunt serve
+fi
