65717bdc3dfcf9713089fa199c9e6af33f5c4dd6
Richer Balázs <richer.balazs@gmail.com>
2016-11-19 14:58:21 +0100

    Refaktor variable name.

--- public/scripts/model/GameModel.js
+++ public/scripts/model/GameModel.js
@@ -1,7 +1,7 @@
 /* global define, $*/
 define('GameModel', function () {
   return function GameModel(ko, myLineChart) {
-    var _this = this;
+    var self = this;
 
     this.games = [101, 301, 501];
 
@@ -38,41 +38,41 @@ define('GameModel', function () {
 
       var scoresToChart = [];
 
-      ko.utils.arrayForEach(_this.players(), function (item) {
+      ko.utils.arrayForEach(self.players(), function (item) {
         scoresToChart.push(item.roundAvg());
       });
 
-      _this.chart.addData(scoresToChart, '');
-      _this.chartCount++;
+      self.chart.addData(scoresToChart, '');
+      self.chartCount++;
 
-      if (_this.chartCount >= 25) {
-        _this.chartCount--;
-        _this.chart.removeData();
+      if (self.chartCount >= 25) {
+        self.chartCount--;
+        self.chart.removeData();
       }
 
       currentPlayer.requireByTurns = currentPlayer.require();
 
-      nextPlayerIndex = _this.currentPlayerIndex + 1;
+      nextPlayerIndex = self.currentPlayerIndex + 1;
 
-      if (nextPlayerIndex >= _this.players().length) {
+      if (nextPlayerIndex >= self.players().length) {
         nextPlayerIndex = 0;
       }
 
-      _this.thrown(0);
+      self.thrown(0);
       currentPlayer.status(0);
 
-      nextInLine = _this.players()[nextPlayerIndex];
+      nextInLine = self.players()[nextPlayerIndex];
       nextInLine.status(1);
 
-      _this.currentPlayerIndex = nextPlayerIndex;
+      self.currentPlayerIndex = nextPlayerIndex;
     };
 
     this.handleThrow = function (scoreOfThrow, id) {
       var currentPlayer;
 
-      currentPlayer = _this.getCurrentPlayer();
+      currentPlayer = self.getCurrentPlayer();
 
-      var ifGameShot = _this.checkForGameShot(currentPlayer, _this.thrown(), _this.switchToDoubleOut());
+      var ifGameShot = self.checkForGameShot(currentPlayer, self.thrown(), self.switchToDoubleOut());
 
       if (ifGameShot) {
         currentPlayer.gameShotAttempnts(currentPlayer.gameShotAttempnts() + 1);
@@ -81,41 +81,41 @@ define('GameModel', function () {
       currentPlayer.history.push(parseInt(scoreOfThrow));
 
       if (currentPlayer.require() < 0) {
-        _this.thrown(_this.thrown() + 1);
-        currentPlayer.history.splice((0 - _this.thrown()), _this.thrown());
-        _this.jumpToNextPlayer(currentPlayer);
+        self.thrown(self.thrown() + 1);
+        currentPlayer.history.splice((0 - self.thrown()), self.thrown());
+        self.jumpToNextPlayer(currentPlayer);
       } else if (currentPlayer.require() === 0) {
-        if (!_this.switchToDoubleOut()) {
-          _this.turnScore(currentPlayer);
-          _this.winner(currentPlayer);
-        } else if (_this.switchToDoubleOut()) {
+        if (!self.switchToDoubleOut()) {
+          self.turnScore(currentPlayer);
+          self.winner(currentPlayer);
+        } else if (self.switchToDoubleOut()) {
           if (id[0] === 'd' || id === 'Bull') {
-            _this.turnScore(currentPlayer);
-            _this.winner(currentPlayer);
+            self.turnScore(currentPlayer);
+            self.winner(currentPlayer);
           } else {
-            _this.thrown(_this.thrown() + 1);
-            currentPlayer.history.splice((0 - _this.thrown()), _this.thrown());
-            _this.jumpToNextPlayer(currentPlayer);
+            self.thrown(self.thrown() + 1);
+            currentPlayer.history.splice((0 - self.thrown()), self.thrown());
+            self.jumpToNextPlayer(currentPlayer);
           }
         }
       } else {
-        if (_this.switchToDoubleOut() === 1 && currentPlayer.require() === 1) {
-          _this.thrown(_this.thrown() + 1);
-          currentPlayer.history.splice((0 - _this.thrown()), _this.thrown());
-          _this.jumpToNextPlayer(currentPlayer);
+        if (self.switchToDoubleOut() === 1 && currentPlayer.require() === 1) {
+          self.thrown(self.thrown() + 1);
+          currentPlayer.history.splice((0 - self.thrown()), self.thrown());
+          self.jumpToNextPlayer(currentPlayer);
         } else {
-          if (_this.thrown() === 2) {
-            _this.turnScore(currentPlayer);
-            _this.jumpToNextPlayer(currentPlayer);
+          if (self.thrown() === 2) {
+            self.turnScore(currentPlayer);
+            self.jumpToNextPlayer(currentPlayer);
           } else {
-            _this.thrown(_this.thrown() + 1);
+            self.thrown(self.thrown() + 1);
           }
         }
       }
     };
 
     this.winner = function (currentPlayer) {
-      if (_this.players().length === 1) {
+      if (self.players().length === 1) {
         currentPlayer.victories(currentPlayer.victories() + 1);
         return;
       }
@@ -124,7 +124,7 @@ define('GameModel', function () {
         currentPlayer.highestGameShot(currentPlayer.requireByTurns);
       }
 
-      ko.utils.arrayForEach(_this.players(), function (player) {
+      ko.utils.arrayForEach(self.players(), function (player) {
         player.status(2);
         player.history([]);
         player.turnHistory([]);
@@ -132,23 +132,23 @@ define('GameModel', function () {
         player.requireByTurns = player.require();
       });
 
-      _this.thrown(0);
+      self.thrown(0);
 
-      _this.currentPlayerIndex = _this.nextPlayerToThrowFirst;
-      _this.players()[_this.currentPlayerIndex].status(1);
-      _this.players()[_this.currentPlayerIndex].firstToThrow(true);
+      self.currentPlayerIndex = self.nextPlayerToThrowFirst;
+      self.players()[self.currentPlayerIndex].status(1);
+      self.players()[self.currentPlayerIndex].firstToThrow(true);
 
-      _this.nextPlayerToThrowFirst++;
+      self.nextPlayerToThrowFirst++;
 
-      if (_this.nextPlayerToThrowFirst >= _this.players().length) {
-        _this.nextPlayerToThrowFirst = 0;
+      if (self.nextPlayerToThrowFirst >= self.players().length) {
+        self.nextPlayerToThrowFirst = 0;
       }
 
       currentPlayer.victories(currentPlayer.victories() + 1);
     };
 
     this.turnScore = function (currentPlayer) {
-      var countDarts = _this.thrown() + 1;
+      var countDarts = self.thrown() + 1;
       var turnThrows = currentPlayer.history.slice(-countDarts);
       var turnSum = 0;
       for (var i = 0; i < countDarts; i++) {
@@ -181,13 +181,13 @@ define('GameModel', function () {
     };
 
     this.swapScore = function () {
-      _this.gameIndex++;
+      self.gameIndex++;
 
-      if (_this.gameIndex >= _this.games.length) {
-        _this.gameIndex = 0;
+      if (self.gameIndex >= self.games.length) {
+        self.gameIndex = 0;
       }
 
-      ko.utils.arrayForEach(_this.players(), function (player) {
+      ko.utils.arrayForEach(self.players(), function (player) {
         player.status(2);
         player.history([]);
         player.turnHistory([]);
@@ -199,38 +199,38 @@ define('GameModel', function () {
       var style;
       var $switchable = $('#switchable');
       var link = $switchable.attr('href');
-      if (_this.switchViewIndex() === 1) {
+      if (self.switchViewIndex() === 1) {
         style = link.replace('view', 'main');
-        _this.switchViewIndex(0);
-      } else if (_this.switchViewIndex() === 0) {
+        self.switchViewIndex(0);
+      } else if (self.switchViewIndex() === 0) {
         style = link.replace('main', 'view');
-        _this.switchViewIndex(1);
+        self.switchViewIndex(1);
       }
       $switchable.attr('href', style);
     };
 
     this.undo = function () {
-      if (_this.thrown() === 0) {
-        _this.getCurrentPlayer().status(2);
+      if (self.thrown() === 0) {
+        self.getCurrentPlayer().status(2);
 
-        _this.currentPlayerIndex--;
-        if (_this.currentPlayerIndex < 0) {
-          _this.currentPlayerIndex = (_this.players().length - 1);
+        self.currentPlayerIndex--;
+        if (self.currentPlayerIndex < 0) {
+          self.currentPlayerIndex = (self.players().length - 1);
         }
 
-        _this.getCurrentPlayer().history.splice(-3, 3);
-        _this.getCurrentPlayer().turnHistory.splice(-1, 1);
-        _this.getCurrentPlayer().allTurnHistory.splice(-1, 1);
+        self.getCurrentPlayer().history.splice(-3, 3);
+        self.getCurrentPlayer().turnHistory.splice(-1, 1);
+        self.getCurrentPlayer().allTurnHistory.splice(-1, 1);
       } else {
-        _this.getCurrentPlayer().history.splice((0 - _this.thrown()), _this.thrown());
-        _this.thrown(0);
+        self.getCurrentPlayer().history.splice((0 - self.thrown()), self.thrown());
+        self.thrown(0);
       }
 
-      _this.getCurrentPlayer().status(1);
+      self.getCurrentPlayer().status(1);
     };
 
     this.getCurrentPlayer = function () {
-      return _this.players()[_this.currentPlayerIndex];
+      return self.players()[self.currentPlayerIndex];
     };
   };
 });
