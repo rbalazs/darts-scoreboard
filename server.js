9a149d08c7bb67e63728986a2b62631c17f3009f
Richer Balázs <richer.balazs@gmail.com>
2016-11-26 19:44:42 +0100

    Remove console log.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
-console.log("asd");
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
